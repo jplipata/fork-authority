d0a748ebcc7777db90f0a2e3e98cb78a6ac2f4eb
JP Lipata <jeremiah.jpl@gmail.com>
2019-11-10 10:30:33 -0500

    View Poll feature. Initial set up. Create Activity and subscribe to a poll (temp backend data).

---
+++ app/src/main/java/com/lipata/forkauthority/poll/FirebaseModel.kt
@@ -0,0 +1,25 @@
+package com.lipata.forkauthority.poll
+
+import com.google.firebase.Timestamp
+import com.google.firebase.firestore.ServerTimestamp
+
+/**
+ * Remember this for Firestore data classes: Properties must have default values
+ * https://stackoverflow.com/a/51134217/4840115
+ *
+ * @property created This is val because we probably don't want anyone changing it.  Other
+ * properties are var for ease of making updates.
+ */
+data class Poll(
+    @ServerTimestamp
+    val created: Timestamp? = null,
+    var restaurants: MutableList<VotableRestaurant> = mutableListOf()
+)
+
+data class VotableRestaurant(
+    var name: String = "",
+    var votesFor: MutableList<String> = mutableListOf(),
+    var votesAgainst: MutableList<String> = mutableListOf()
+) {
+    fun totalVotes() = votesFor.size - votesAgainst.size
+}
--- app/src/main/java/com/lipata/forkauthority/poll/PollActivity.kt
+++ app/src/main/java/com/lipata/forkauthority/poll/PollActivity.kt
@@ -2,11 +2,59 @@ package com.lipata.forkauthority.poll
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ListenerRegistration
 import com.lipata.forkauthority.R
+import kotlinx.android.synthetic.main.activity_poll.*
 
 class PollActivity : AppCompatActivity() {
+    private val adapter = PollAdapter()
+    lateinit var viewModel: PollViewModel
+    lateinit var registration: ListenerRegistration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_poll)
+
+        pollRecycler.run {
+            setHasFixedSize(true)
+            layoutManager = LinearLayoutManager(this@PollActivity)
+            adapter = this@PollActivity.adapter
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        viewModel.onStart()
+
+        val db = FirebaseFirestore.getInstance()
+        subscribeToPoll(db)
+
+    }
+
+    private fun subscribeToPoll(db: FirebaseFirestore) {
+        val docRef = db.collection("polls").document("4z4zop9XAhkJ0dSeuVXm") // TODO Hardcoded temporary poll
+        registration = docRef.addSnapshotListener { snapshot, e ->
+            if (e != null) {
+                Snackbar.make(pollRecycler, "Listen failed. $e", Snackbar.LENGTH_LONG)
+                return@addSnapshotListener
+            }
+
+            if (snapshot != null && snapshot.exists()) {
+                val poll = snapshot.toObject(Poll::class.java)
+                adapter.items = poll?.restaurants.orEmpty().sortedByDescending { votableRestaurant -> votableRestaurant.totalVotes() }
+                adapter.notifyDataSetChanged()
+            } else {
+                Snackbar.make(pollRecycler, "Current data: null", Snackbar.LENGTH_LONG)
+            }
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        registration.remove()
     }
 }
