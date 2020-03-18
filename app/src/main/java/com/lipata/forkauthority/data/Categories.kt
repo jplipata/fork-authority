848659036bc4c6f2307c8789b7b1dcbcda1ca997
JP Lipata <jeremiah.jpl@gmail.com>
2020-03-18 19:22:22 -0400

    Change too soon text to just ate here

--- app/src/main/java/com/lipata/forkauthority/data/Categories.kt
+++ app/src/main/java/com/lipata/forkauthority/data/Categories.kt
@@ -3,9 +3,9 @@ package com.lipata.forkauthority.data
 class Categories {
     companion object {
         val LIKES = "Likes"
-        val LIKES_TOO_SOON = "Liked, But Too Soon"
+        val LIKES_TOO_SOON = "Liked, But Just Ate Here"
         val UNSORTED = "Unsorted"
-        val UNSORTED_TOO_SOON = "Unsorted, Too Soon"
+        val UNSORTED_TOO_SOON = "Unsorted, Just Ate Here"
         val DONT_LIKE = "Don't Like"
     }
 }
