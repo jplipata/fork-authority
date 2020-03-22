166ab0ac2938f04504030ff0798faf82af2c1c88
JP Lipata <jeremiah.jpl@gmail.com>
2020-03-22 14:47:59 -0400

    Refactor ExpirationProvider

--- app/src/main/java/com/lipata/forkauthority/di/AppModule.kt
+++ app/src/main/java/com/lipata/forkauthority/di/AppModule.kt
@@ -3,13 +3,12 @@ package com.lipata.forkauthority.di
 import android.app.Application
 import android.content.Context
 import android.content.SharedPreferences
-import androidx.preference.PreferenceManager
 import com.lipata.forkauthority.R
 import com.lipata.forkauthority.businesslist.ExpirationProvider
+import com.lipata.forkauthority.businesslist.JustAteHereExpirationProviderImpl
 import com.lipata.forkauthority.util.AddressParser
 import dagger.Module
 import dagger.Provides
-import timber.log.Timber
 
 @Module
 class AppModule(private val application: Application) {
@@ -43,21 +42,6 @@ class AppModule(private val application: Application) {
     @Provides
     @ApplicationScope
     fun provideJustAteHerePref(context: Context): ExpirationProvider {
-        return object : ExpirationProvider {
-            override fun get(): Int {
-                val default = context.getString(R.string.preference_default_value_just_ate_here_expiration)
-
-                val expirationPrefString = PreferenceManager.getDefaultSharedPreferences(context).getString(
-                    context.getString(R.string.preference_key_just_ate_here_expiration),
-                    default) ?: default
-
-                return try {
-                    expirationPrefString.toInt()
-                } catch (e: Exception) {
-                    Timber.e(e)
-                    default.toInt()
-                }
-            }
-        }
+        return JustAteHereExpirationProviderImpl(context)
     }
 }
